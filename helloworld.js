7580bd49c01d368cba433c0d94d1423775486a31
David Louis <davloufl@gmail.com>
2024-01-23 13:04:56 -0600

    Finished the demo

---
+++ helloworld.js
@@ -0,0 +1,34 @@
+console.log('Hello World')
+let firstName = 'David'
+console.log(firstName) 
+let age = 28
+
+console.log(`Hello my name is ${firstName} and I am ${age} years old.`)
+
+const isCool = true 
+const hasCats = true
+
+let height;
+let height2 = undefined
+
+height = '5ft, 10in'
+const num1=6
+const num2=4.2
+console.log('Original: ',num1 + num2)
+console.log(Math.round(num1 + num2))
+console.log(height)
+// This comment is merely a comment. Remember ctrl + ? !
+
+let scottAge = 25
+
+if (age > scottAge) {
+    console.log('I am older than Scott!')
+    } else if (age === scottAge) {
+        console.log('Holy Moly this is impossssssiibbbllelelelelel')
+    } else {
+        console.log(`I am a wee baby when compared to ye ol' Scott`)
+    } 
+    
+    if(age !== scottAge) {
+        console.log('Scott is not teh same age as me that lucky ducky...')
+    }
